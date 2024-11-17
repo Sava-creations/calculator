75e18d68275dbe4fbe0a6a7d7126f616347e33f0
Savandi <savandikodithuwakku@gmail.com>
2024-11-18 03:00:53 +0530

    Errors have been corrected

--- script.js
+++ script.js
@@ -24,26 +24,54 @@ function setoperator(op) {
     if (currentString === ''){ //avoid showing operators without any digit
         return;
     }
-    
-    if (['+', '-', '*', '/'].includes(currentString.slice(-1))) { // Check if the last character in currentString is an operator
+    if (['+', '-', '*', '/','%'].includes(currentString[currentString.length - 1])) { // Check if the last character in currentString is an operator
         currentString = currentString.slice(0, -1); // Remove the last operator
     }
     currentString+=op;
     document.getElementById('result'). value= currentString;
 }
 
-function brackets(){
-    if (currentString.includes('(')){
-        currentString+=')';
-    }else{
-        currentString+='(';
+function brackets() {
+    // Check if the last character is an open bracket
+    if (currentString === '' || currentString.slice(-1) === '(' || ['+', '-', '*', '/'].includes(currentString.slice(-1))) {
+        currentString += '(';
+    } else {
+        currentString += ')';
     }
-    document.getElementById('result').value= currentString;
+
+    document.getElementById('result').value = currentString;
 }
+
+
 function _calculate(){
+    try{
+    if(currentString[currentString.length - 1] === '%')
+        setpercent();
+    else{
+    //let openBrackets = currentString;
     let result=eval(currentString);
- 
+    
     result = parseFloat(result.toFixed(10)); // fix precision error by converting number to sring with 10 digits and then back to a number
-        document.getElementById('result').value = result;
-        currentString = result.toString();
+    document.getElementById('result').value = result;
+    currentString = result.toString();
+    }
+    }
+    catch(error){
+        document.getElementById('result').value = 'Error';
+        currentString = '';
+}}
+function setpercent() {
+    // if (['+', '-', '*', '/',].includes(currentString.slice(-1))) { // Check if the last character in currentString is an operator
+    //     currentString = currentString.slice(0, -1); // Remove the last operator
+    // }
+    let value=parseFloat(currentString.slice(0,-1))/100;
+    document.getElementById('result').value= value;
+    currentString = result.toString();
+}
+
+function oneClear() {
+    //if(currentString.length>0){
+        currentString=currentString.slice(0,-1);
+        document.getElementById('result').value= currentString||0;//if currentString is empty it will show 0 if it '' empty string or null as they are also falsy values it will show 0
+   // }
 }
