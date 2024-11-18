dc5b292265afc836e35278b1abcd46443d7305ef
Savandi <savandikodithuwakku@gmail.com>
2024-11-18 18:49:51 +0530

    updated the code to handle brackets

--- script.js
+++ script.js
@@ -32,34 +32,20 @@ function setoperator(op) {
 }
 
 function brackets() {
-    // Check if the last character is an open bracket
-    if (currentString === '' || currentString.slice(-1) === '(' || ['+', '-', '*', '/'].includes(currentString.slice(-1))) {
-        currentString += '(';
-    } else {
-        currentString += ')';
-    }
+    
+    const openCount = (currentString.match(/\(/g) || []).length;    //count of open brackets
+    const closeCount = (currentString.match(/\)/g) || []).length;
 
+    if (currentString === '' || ['+', '-', '*', '/', '('].includes(currentString.slice(-1))) {       // Check if the last character is an open bracket
+        currentString += '(';
+    } else if (openCount > closeCount) {
+        currentString += ')';
+    } else {
+        currentString += '(';
+    }
     document.getElementById('result').value = currentString;
 }
 
-
-function _calculate(){
-    try{
-    if(currentString[currentString.length - 1] === '%')
-        setpercent();
-    else{
-    //let openBrackets = currentString;
-    let result=eval(currentString);
-    
-    result = parseFloat(result.toFixed(10)); // fix precision error by converting number to sring with 10 digits and then back to a number
-    document.getElementById('result').value = result;
-    currentString = result.toString();
-    }
-    }
-    catch(error){
-        document.getElementById('result').value = 'Error';
-        currentString = '';
-}}
 function setpercent() {
     // if (['+', '-', '*', '/',].includes(currentString.slice(-1))) { // Check if the last character in currentString is an operator
     //     currentString = currentString.slice(0, -1); // Remove the last operator
@@ -75,3 +61,21 @@ function oneClear() {
         document.getElementById('result').value= currentString||0;//if currentString is empty it will show 0 if it '' empty string or null as they are also falsy values it will show 0
    // }
 }
+
+function _calculate(){
+    try{
+    if(currentString[currentString.length - 1] === '%')
+        setpercent();
+    else{
+
+    let result=eval(currentString);
+    
+    result = parseFloat(result.toFixed(10)); // fix precision error by converting number to sring with 10 digits and then back to a number
+    document.getElementById('result').value = result;
+    currentString = result.toString();
+    }
+    }
+    catch(error){
+        document.getElementById('result').value = 'Error';
+        currentString = '';
+}}
